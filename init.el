e627a6c870ed834f8f29d86b840a67c3e66f767b
Ermanno Moser <ermann@amazon.com>
2020-02-28 18:47:21 +0100

    Setup magit

--- init.el
+++ init.el
@@ -121,3 +121,20 @@
   (global-set-key (kbd "C-c i") 'counsel-imenu)
   (define-key minibuffer-local-map (kbd "C-r") 'counsel-minibuffer-history))
 
+;; Magit
+(use-package magit
+  :ensure t
+  :defer t
+  :bind (("C-x g" . magit-status))
+  :config
+  (progn
+    (defun ermann/magit-log-edit-mode-hook ()
+      (setq fill-column 72)
+      (flyspell-mode t)
+      (turn-on-auto-fill))
+    (add-hook 'magit-log-edit-mode-hook 'ermann/magit-log-edit-mode-hook)
+    (defadvice magit-status (around magit-fullscreen activate)
+      (window-configuration-to-register :magit-fullscreen)
+      ad-do-it
+      (delete-other-windows))))
+
