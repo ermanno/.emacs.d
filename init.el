ea4c6af8ca293e17a6b9b5d606f85cb6ebdd7e0a
Ermanno Moser <ermann@amazon.com>
2020-02-28 17:34:16 +0100

    Setup package management
    Add MELPA repository and install use-package if it is not already
    installed.

--- init.el
+++ init.el
@@ -3,3 +3,20 @@
       `((".*" . ,temporary-file-directory)))
 (setq auto-save-file-name-transforms
       `((".*" ,temporary-file-directory t)))
+
+(package-initialize)
+(setq use-package-always-ensure t)
+
+;; GNU ELPA is already part of package-archives, add melpa
+(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/") t)
+(package-refresh-contents)
+
+;; Install use-package if it is not already installed
+(unless (package-installed-p 'use-package)
+  (package-install 'use-package))
+(setq use-package-verbose t)
+(setq use-package-always-ensure t)
+(require 'use-package)
+(use-package auto-compile
+	     :config (auto-compile-on-load-mode))
+(setq load-prefer-newer t)
