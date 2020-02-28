9811a99e6b4793ffd557474f71f8f875bdf6647f
Ermanno Moser <ermann@amazon.com>
2020-02-28 19:00:52 +0100

    Add Markdown support

--- init.el
+++ init.el
@@ -154,6 +154,15 @@
 (use-package yaml-mode
   :mode "\\.yaml\\'")
 
+;; Markdown support
+(use-package markdown-mode
+  :ensure t
+  :commands (markdown-mode gfm-mode)
+  :mode (("README\\.md\\'" . gfm-mode)
+         ("\\.md\\'" . markdown-mode)
+         ("\\.markdown\\'" . markdown-mode))
+  :init (setq markdown-command "multimarkdown"))
+
 ;; Org mode
 (require 'org-mouse)
 
