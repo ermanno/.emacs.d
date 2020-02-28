d187f0e37a49af74f712eb1b64563874af36066f
Ermanno Moser <ermann@amazon.com>
2020-02-28 18:45:34 +0100

    Setup multiple cursors

--- init.el
+++ init.el
@@ -74,3 +74,15 @@
   :ensure t
   :bind ("C-c t" . transpose-frame))
 
+;; Multiple cursors
+(use-package multiple-cursors
+  :init
+  (progn
+    ;; these need to be defined here - if they're lazily loaded with
+    ;; :bind they don't work.
+    (global-set-key (kbd "C-c .") 'mc/mark-next-like-this)
+    (global-set-key (kbd "C->") 'mc/mark-next-like-this)
+    (global-set-key (kbd "C-c ,") 'mc/mark-previous-like-this)
+    (global-set-key (kbd "C-<") 'mc/mark-previous-like-this)
+    (global-set-key (kbd "C-c C-l") 'c/mark-all-like-this)))
+
