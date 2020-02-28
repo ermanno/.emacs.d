26f68d2c142cff5eb9030b61c932d55532396e44
Ermanno Moser <ermann@amazon.com>
2020-02-28 19:03:28 +0100

    Add new packages to custom-settings.el

--- custom-settings.el
+++ custom-settings.el
@@ -5,7 +5,12 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (expand-region neotree counsel smex swiper ivy undo-tree projectile auto-compile use-package))))
+    (markdown-mode yaml-mode expand-region neotree counsel smex swiper ivy undo-tree projectile auto-compile use-package)))
+ '(sml/name-width 32)
+ '(sml/no-confirm-load-theme t)
+ '(sml/replacer-regexp-list nil)
+ '(sml/shorten-modes nil)
+ '(sml/theme (quote light)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
