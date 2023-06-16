0c8524eb6e9c22d13fc9f89e09b3855456b171c7
ermanno <moser.ermanno@gmail.com>
2023-06-16 22:51:36 +0200

    Clean up multiple-cursors initialization

--- init.el
+++ init.el
@@ -99,15 +99,11 @@
 
 ;; Multiple cursors
 (use-package multiple-cursors
-  :init
-  (progn
-    ;; these need to be defined here - if they're lazily loaded with
-    ;; :bind they don't work.
-    (global-set-key (kbd "C-c .") 'mc/mark-next-like-this)
-    (global-set-key (kbd "C->") 'mc/mark-next-like-this)
-    (global-set-key (kbd "C-c ,") 'mc/mark-previous-like-this)
-    (global-set-key (kbd "C-<") 'mc/mark-previous-like-this)
-    (global-set-key (kbd "C-c C-l") 'c/mark-all-like-this)))
+  :bind (("C->" . mc/mark-next-like-this)
+         ("C-<" . mc/mark-previous-like-this)
+         ("C-c ." . mc/mark-next-like-this)
+         ("C-c ," . mc/mark-previous-like-this)
+         ("C-c C-l" . mc/mark-all-like-this)))
 
 ;; Ivy/Swiper/Counsel/Smex
 (use-package ivy
