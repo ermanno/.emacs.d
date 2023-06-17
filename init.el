2d968df1cef10d1adadf29e18ef8d74d95a82adc
ermanno <moser.ermanno@gmail.com>
2023-06-17 20:08:47 +0200

    Simplify TypeScript mode (Tide)
    This patch simplifies Tide's setup, according to the configuration
    presented in https://github.com/ananthakumaran/tide

--- init.el
+++ init.el
@@ -249,41 +249,15 @@
   :mode (("\\.php$" .  web-mode)
          ("\\.html$" .  web-mode)))
 
-;; TODO company and flycheck are generic packages whose configuration
-;; should live somewhere else than tide's
-
-(use-package tide)
 (use-package company)
 (use-package flycheck)
 
-(defun setup-tide-mode ()
-  (interactive)
-  (tide-setup)
-  (flycheck-mode +1)
-  (setq flycheck-check-syntax-automatically '(save mode-enabled))
-  (eldoc-mode +1)
-  (tide-hl-identifier-mode +1)
-  (company-mode +1))
-
-;; aligns annotation to the right hand side
-(setq company-tooltip-align-annotations t)
-
-;; formats the buffer before saving
-(add-hook 'before-save-hook 'tide-format-before-save)
-
-(add-hook 'typescript-mode-hook #'setup-tide-mode)
-
-;;;;; Make Tide aware of tsx files
-(require 'web-mode)
-
-(add-to-list 'auto-mode-alist '("\\.tsx\\'" . web-mode))
-(add-hook 'web-mode-hook
-          (lambda ()
-            (when (string-equal "tsx" (file-name-extension buffer-file-name))
-              (setup-tide-mode))))
-
-;; enable typescript - tslint checker
-(flycheck-add-mode 'typescript-tslint 'web-mode)
+;; TypeScript (https://github.com/ananthakumaran/tide)
+(use-package tide
+  :after (typescript-mode company flycheck)
+  :hook ((typescript-mode . tide-setup)
+         (typescript-mode . tide-hl-identifier-mode)
+         (before-save . tide-format-before-save)))
 
 ;; Org mode
 (require 'org-mouse)
