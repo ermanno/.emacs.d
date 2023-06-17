3c28417cd0bc4002967741bfa9318c71cf4f962d
ermanno <moser.ermanno@gmail.com>
2023-06-18 00:12:41 +0200

    Refactor code for Mac keybindings when not attached to a monitor

--- init.el
+++ init.el
@@ -28,11 +28,16 @@
 (setq load-prefer-newer t)
 
 ;; Mac settings
+
+(defun ermann/attached-to-external-monitor-p ()
+  "Check whether the laptop is attached to an external monitor"
+  (string-match-p
+   "[Kk]eyboard"
+   (shell-command-to-string "ioreg -p IOUSB -w0")))
+
 (when (and
        (eq system-type 'darwin)
-       (not (string-match-p
-             "[Kk]eyboard"
-             (shell-command-to-string "ioreg -p IOUSB -w0"))))
+       (not (ermann/attached-to-external-monitor-p)))
   (setq mac-command-modifier 'meta
         mac-option-modifier 'control))
 
