decb0f3003142fe5f58d6939002c723a7df4a6ef
ermann <moser.ermanno@gmail.com>
2023-07-30 15:52:24 +0200

    Move UI config to a separate file

---
+++ config/ui-config.el
@@ -0,0 +1,29 @@
+(setq inhibit-startup-screen t
+      frame-title-format "%b"
+      visible-bell 1)
+(scroll-bar-mode -1)
+(tool-bar-mode -1)
+(column-number-mode)
+(show-paren-mode)
+
+(use-package zenburn-theme
+  :config
+  (load-theme 'zenburn t)
+  (set-face-attribute 'default nil :height 150))
+
+(use-package hl-todo
+  :config (global-hl-todo-mode))
+
+(use-package rainbow-delimiters
+  :hook (prog-mode . rainbow-delimiters-mode))
+
+(use-package smart-mode-line
+  :custom
+  (sml/no-confirm-load-theme t)
+  (sml/theme 'automatic)
+  (sml/name-width 32)
+  (sml/shorten-modes nil)
+  (sml/replacer-regexp-list nil)
+  :config (sml/setup))
+
+(provide 'ui-config)
--- init.el
+++ init.el
@@ -32,34 +32,7 @@
   :config (auto-compile-on-load-mode))
 (setq load-prefer-newer t)
 
-;; UI
-(setq inhibit-startup-screen t
-      frame-title-format "%b"
-      visible-bell 1)
-(scroll-bar-mode -1)
-(tool-bar-mode -1)
-(column-number-mode)
-(show-paren-mode)
-
-(use-package zenburn-theme
-  :config
-  (load-theme 'zenburn t)
-  (set-face-attribute 'default nil :height 150))
-
-(use-package hl-todo
-  :config (global-hl-todo-mode))
-
-(use-package rainbow-delimiters
-  :hook (prog-mode . rainbow-delimiters-mode))
-
-(use-package smart-mode-line
-  :custom
-  (sml/no-confirm-load-theme t)
-  (sml/theme 'automatic)
-  (sml/name-width 32)
-  (sml/shorten-modes nil)
-  (sml/replacer-regexp-list nil)
-  :config (sml/setup))
+(require 'ui-config)
 
 ;; UX
 (global-auto-revert-mode)
