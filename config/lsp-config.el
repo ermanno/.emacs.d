47284fb7d556900716a10aeff8347a7b8cc255b6
ermann <moser.ermanno@gmail.com>
2025-01-18 17:47:48 +0100

    Fix mode I want to start lsp in

--- config/lsp-config.el
+++ config/lsp-config.el
@@ -2,7 +2,7 @@
 (use-package lsp-mode
   :init
   (setq lsp-keymap-prefix "C-c l")
-  :hook ((go-mode . lsp)
+  :hook ((go-ts-mode . lsp)
          (lsp-mode . lsp-enable-which-key-integration))
   :commands lsp)
 
