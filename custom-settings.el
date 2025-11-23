9a8b3b6b59c82826418b5346b2dff55dda318929
ermanno <moser.ermanno@gmail.com>
2025-11-23 14:15:49 +0100

    Update custom-settings.el

--- custom-settings.el
+++ custom-settings.el
@@ -3,7 +3,16 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages nil)
+ '(package-selected-packages
+   '(auto-compile company counsel dap-mode elfeed exec-path-from-shell
+                  expand-region flycheck-plantuml flycheck-rust
+                  geiser-mit git-gutter-fringe hl-todo lsp-ivy
+                  lsp-pyright lsp-ui magit modus-themes
+                  multiple-cursors org-anki org-download org-pomodoro
+                  perspective projectile rainbow-delimiters
+                  smart-mode-line smartparens smex tide
+                  transpose-frame treesit-auto undo-tree unfill vterm
+                  web-mode yaml-mode yasnippet-snippets))
  '(python-shell-interpreter "python3")
  '(warning-suppress-types '((comp))))
 (custom-set-faces
