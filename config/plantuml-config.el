42876dfd82371eab73de7a8414cb3739918cae58
ermanno <moser.ermanno@gmail.com>
2025-11-27 23:25:56 +0100

    Update PlantUML jar

--- config/plantuml-config.el
+++ config/plantuml-config.el
@@ -5,7 +5,7 @@
   :custom
   (plantuml-jar-path "~/bin/plantuml-1.2022.0.jar")
   (plantuml-default-exec-mode 'jar)
-  (org-plantuml-jar-path (expand-file-name "~/bin/plantuml-1.2022.0.jar"))
+  (org-plantuml-jar-path (expand-file-name "~/bin/plantuml-1.2025.10.jar"))
   (org-startup-with-inline-images t))
 
 (use-package flycheck-plantuml
