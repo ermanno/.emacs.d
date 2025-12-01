c00d691d514bbdbc89bbef73b2c533eb976b4e81
Ermanno Moser <ermann@amazon.com>
2025-12-01 22:08:15 +0100

    Updated custom settings

--- custom-settings.el
+++ custom-settings.el
@@ -4,16 +4,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(auto-compile company counsel dap-mode elfeed exec-path-from-shell
-                  expand-region flycheck-plantuml flycheck-rust
-                  geiser-mit git-gutter-fringe hl-todo htmlize lsp-ivy
-                  lsp-pyright lsp-ui magit modus-themes
-                  multiple-cursors ob-rust org-anki org-download
-                  org-pomodoro perspective projectile
-                  rainbow-delimiters rust-mode smart-mode-line
-                  smartparens smex tide transpose-frame treesit-auto
-                  undo-tree unfill vterm web-mode yaml-mode
-                  yasnippet-snippets))
+   '(which-key auto-compile company counsel dap-mode elfeed exec-path-from-shell expand-region flycheck-plantuml flycheck-rust geiser-mit git-gutter-fringe hl-todo htmlize lsp-ivy lsp-pyright lsp-ui magit modus-themes multiple-cursors ob-rust org-anki org-download org-pomodoro perspective projectile rainbow-delimiters rust-mode smart-mode-line smartparens smex tide transpose-frame treesit-auto undo-tree unfill vterm web-mode yaml-mode yasnippet-snippets))
  '(python-shell-interpreter "python3")
  '(warning-suppress-types '((comp))))
 (custom-set-faces
