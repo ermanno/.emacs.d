9a2313181cb86d4d9ff06ce22203312bc3e2e32c
ermanno <moser.ermanno@gmail.com>
2026-05-12 02:46:45 +0200

    Improve Tramp config
    This patch improves Tramp config based on the information in
    https://stackoverflow.com/questions/13997965/bizzarre-emacs-tramp-fix

--- config/tramp-config.el
+++ config/tramp-config.el
@@ -2,7 +2,7 @@
 (use-package tramp
   :config
   ;; To speed up connections
-  (setq tramp-verbose 0
+  (setq tramp-verbose 10 ; https://stackoverflow.com/questions/13997965/bizzarre-emacs-tramp-fix
         tramp-chunksize 2000
         tramp-use-ssh-controlmaster-options nil
         tramp-default-method "ssh"
@@ -10,7 +10,11 @@
         tramp-default-remote-shell "/bin/sh"
         tramp-connection-local-default-shell-variables
         '((shell-file-name . "/bin/bash")
-          (shell-command-switch . "-c")))
+          (shell-command-switch . "-c"))
+        tramp-auto-save-directory temporary-file-directory)
+  ;; Backup (file~) disabled and auto-save (#file#) locally to prevent delays in editing remote files
+  (add-to-list 'backup-directory-alist
+               (cons tramp-file-name-regexp nil))
   ;; For ~/.local/bin etc. to work when SSHing
   (add-to-list 'tramp-remote-path 'tramp-own-remote-path))
 
