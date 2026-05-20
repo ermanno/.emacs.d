658bbc6a617d370b4411d4b104b00463ca7a37a8
Ermanno Moser <ermann@amazon.com>
2026-05-20 22:51:18 +0200

    Add instructions to install Tree-sitter for Rust on Mac

--- custom-settings.el
+++ custom-settings.el
@@ -3,8 +3,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   '(vterm exec-path-from-shell ob-rust org-download org-anki htmlize flycheck-rust rust-mode flycheck-plantuml plantuml-mode git-gutter yasnippet-snippets yasnippet geiser-mit smartparens perspective yaml-mode flycheck unfill expand-region counsel smex swiper ivy multiple-cursors transpose-frame undo-tree projectile rainbow-delimiters hl-todo use-package)))
+ '(package-selected-packages nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
